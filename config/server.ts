6b838765be1c5ad3e23ea467139f0f2268c10c30
Peter Browse <peterbrowse@gmail.com>
2026-05-29 01:28:43 +0100

    Set public API URL for generated links

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
-});
+export default ({ env }) => {
+  const publicUrl = env('PUBLIC_URL', undefined);
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    ...(publicUrl ? { url: publicUrl } : {}),
+    app: {
+      keys: env.array('APP_KEYS'),
+    },
+  };
+};
